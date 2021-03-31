c4647399da7a2cb8651c24be90cc9dc165d2fb81
artur <artik222012@gmail.com>
2021-03-31 18:09:18 +0500

    reduce ios targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,36 +10,9 @@ repositories {
 }
 
 kotlin {
-    iosX64 {
-        binaries {
-            framework {
-                baseName = "library"
-            }
-        }
-    }
-    iosArm64 {
-        binaries {
-            framework {
-                baseName = "library"
-            }
-        }
-    }
-    iosArm32 {
-        binaries {
-            framework {
-                baseName = "library"
-            }
-        }
-    }
-    macosX64()
+    ios()
     sourceSets {
-        val iosX64Main by getting
-        val iosX64Test by getting
-        val iosArm64Main by getting
-        val iosArm64Test by getting
-        val iosArm32Main by getting
-        val iosArm32Test by getting
-        val macosX64Main by getting
-        val macosX64Test by getting
+        val iosMain by getting
+        val iosTest by getting
     }
 }
