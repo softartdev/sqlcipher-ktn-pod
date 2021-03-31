05b51b12f0c4b9cd69d684282285f03b45368286
artur <artik222012@gmail.com>
2021-03-31 19:41:07 +0500

    add cocoapods

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.4.32"
+    kotlin("native.cocoapods") version "1.4.32"
 }
 
 group = "com.softartdev"
@@ -14,5 +15,12 @@ kotlin {
     sourceSets {
         val iosMain by getting
         val iosTest by getting
+    }
+    cocoapods {
+        frameworkName = "SQLCipher pod on Kotlin Native"
+        summary = "Wrap SQLCipher lib from Cocoapods to Kotlin Native"
+        homepage = "https://github.com/softartdev/sqlcipher-ktn-pod"
+        ios.deploymentTarget = "13.5"
+        pod("SQLCipher", "~> 4.0")
     }
 }
