53c2812272e66bb107d4749da097c61281648b1d
artur <artik222012@gmail.com>
2021-04-01 07:07:28 +0500

    demodularization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,23 @@
-buildscript {
-    repositories {
-        google()
-        jcenter()
-    }
-    dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.4.32"))
+plugins {
+    kotlin("multiplatform") version "1.4.32"
+    kotlin("native.cocoapods") version "1.4.32"
+    id("maven-publish")
+}
+
+group = "io.github.softartdev"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    ios()
+    cocoapods {
+        frameworkName = "SQLCipher pod on Kotlin Native"
+        summary = "Wrap SQLCipher lib from Cocoapods to Kotlin Native"
+        homepage = "https://github.com/softartdev/sqlcipher-ktn-pod"
+        ios.deploymentTarget = "13.5"
+        pod("SQLCipher", "~> 4.0")
     }
 }
