29a9c7fc749a5c2d4cd4eaaa3070e3a702c55c46
artur <artik222012@gmail.com>
2021-04-08 02:00:37 +0500

    Revert "don't ignore build & gradle folders"
    This reverts commit 8167f687

--- build/cocoapods/synthetic/IOS/sqlcipher_ktn_pod/build/Pods.build/Release-iphoneos/SQLCipher.build/Objects-normal/arm64/SQLCipher-dummy.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/artur/IdeaProjects/sqlcipher-ktn-pod/build/cocoapods/synthetic/IOS/sqlcipher_ktn_pod/Pods/Target\ Support\ Files/SQLCipher/SQLCipher-dummy.m \
-  /Users/artur/IdeaProjects/sqlcipher-ktn-pod/build/cocoapods/synthetic/IOS/sqlcipher_ktn_pod/build/Pods.build/Release-iphoneos/SQLCipher.build/module.modulemap \
-  /Users/artur/IdeaProjects/sqlcipher-ktn-pod/build/cocoapods/synthetic/IOS/sqlcipher_ktn_pod/Pods/Target\ Support\ Files/SQLCipher/SQLCipher-prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/dyld.modulemap
--- build/cocoapods/synthetic/IOS/sqlcipher_ktn_pod/build/Pods.build/Release-iphonesimulator/SQLCipher.build/Objects-normal/x86_64/SQLCipher_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/artur/IdeaProjects/sqlcipher-ktn-pod/build/cocoapods/synthetic/IOS/sqlcipher_ktn_pod/build/Pods.build/Release-iphonesimulator/SQLCipher.build/DerivedSources/SQLCipher_vers.c
