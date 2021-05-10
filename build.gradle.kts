711e66ac28e7f202a59292c12c378bab4b005302
artur <artik222012@gmail.com>
2021-05-10 08:08:19 +0500

    disable commonizer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
     id("convention.publication")
@@ -10,21 +12,37 @@ repositories {
     mavenCentral()
 }
 
-fun configInterop(target: org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget) {
+kotlin {
+    val knTargets = listOf(
+        macosX64(),
+        iosX64(),
+        iosArm64(),
+        iosArm32(),
+        watchosArm32(),
+        watchosArm64(),
+        watchosX86(),
+        watchosX64(),
+        tvosArm64(),
+        tvosX64()
+    )
+    knTargets.forEach(action = ::configInterop)
+
+    sourceSets {
+        val appleMain = sourceSets.maybeCreate("appleMain")
+        val appleTest = sourceSets.maybeCreate("appleTest")
+
+        knTargets.forEach { target ->
+            target.compilations.getByName("main").source(appleMain)
+            target.compilations.getByName("test").source(appleTest)
+        }
+    }
+}
+
+fun configInterop(target: KotlinNativeTarget) {
     val main by target.compilations.getting
     val sqlite3 by main.cinterops.creating {
         includeDirs("$projectDir/src/include")
     }
-}
-
-kotlin {
-//    ios()//TODO use both x64 & arm targets
-    val knTarget = iosX64("ios")
-
-    configInterop(knTarget)
-
-    knTarget.let { target ->
-        val test by target.compilations.getting
-        test.kotlinOptions.freeCompilerArgs += listOf("-linker-options", "-lsqlite3")
-    }
+    val test by target.compilations.getting
+    test.kotlinOptions.freeCompilerArgs += listOf("-linker-options", "-lsqlite3")
 }
